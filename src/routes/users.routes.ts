291ff09ce6b6c7c748b398148f352377b533ceb2
Alejandro <alejandroaperez1994g@gmail.com>
2023-10-10 17:14:57 +0200

    :hammer: added cloudinary route

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,11 @@
 import {Router} from "express";
-import {createUser, getAllUsers} from "../controllers/users.controllers";
+import {createUser, getAllUsers, uploadRequest} from "../controllers/users.controllers";
 
 const usersRoutes = Router()
 
 usersRoutes.get("/", getAllUsers)
 usersRoutes.post("/",createUser)
+usersRoutes.post("/upload", uploadRequest)
 
 
 export default usersRoutes
